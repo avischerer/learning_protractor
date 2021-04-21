84bb6c066db501ce1510bc7208acb3cdca699782
avischerer <42958777+avischerer@users.noreply.github.com>
2021-04-22 00:04:37 +0300

    fixed bugs
    added testing if clicked
    added page is non angular

--- test1.js
+++ test1.js
@@ -1,12 +1,16 @@
 let MyJSON = require("./JSONconfig.json");
+
+
 let txt = JSON.parse(JSON.stringify(MyJSON));
-describe(txt.testname, function () {
-  it("mytest", function () {
-    browser.get(txt.adrees);
-    txt.classes.forEach(function (data) {
+describe(txt.testName, function () {
+  txt.classes.forEach(function (data) {
+    it(data.testDescriptin, function () {
+      browser.get(txt.adrees);
+      expect(browser.getTitle()).toEqual(txt.expetwebpage);
       let testElement = element(by.xpath("//*[@class=" + data.class));
       testElement.click();
+      browser.waitForAngularEnabled(data.isAngular);
+      expect(browser.getTitle()).toEqual(data.expet);
     });
   });
 });
-
