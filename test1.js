c4bfd2536f67143ba5c8f5a02c693e666d60a0c6
avischerer <42958777+avischerer@users.noreply.github.com>
2021-04-26 09:48:18 +0300

    minor bugs

--- test1.js
+++ test1.js
@@ -1,16 +1,16 @@
-let MyJSON = require("./JSONconfig.json");
+const MyJSON = require("./JSONconfig.json");
 
-
-let txt = JSON.parse(JSON.stringify(MyJSON));
+const part1 = "//*[@class=";
+const txt = JSON.parse(JSON.stringify(MyJSON));
 describe(txt.testName, function () {
   txt.classes.forEach(function (data) {
-    it(data.testDescriptin, function () {
-      browser.get(txt.adrees);
-      expect(browser.getTitle()).toEqual(txt.expetwebpage);
-      let testElement = element(by.xpath("//*[@class=" + data.class));
+    it(data.testDescription, function () {
+      browser.get(txt.address);
+      expect(browser.getTitle()).toEqual(txt.expectpagetitle);
+      const testElement = element(by.xpath(part1 + data.class));
       testElement.click();
       browser.waitForAngularEnabled(data.isAngular);
-      expect(browser.getTitle()).toEqual(data.expet);
+      expect(browser.getTitle()).toEqual(data.expect);
     });
   });
 });
